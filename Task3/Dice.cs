24612263289f91ddce8d7a87bda2aa895c896bf5
Roman Saharov <fantomfantomovich0@gmail.com>
2025-05-25 18:12:59 +0500

    Добавьте файлы проекта.

---
+++ Task3/Dice.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3
+{
+    public class Dice
+    {
+        private int[] _faces;
+
+        public int[] Faces => _faces;
+
+        public Dice(string fascesWithSeparator)
+        {
+            var parts = fascesWithSeparator.Split(',');
+            if (parts.Length != 6)
+            {
+
+            }
+        }
+    }
+}
---
+++ Task3/DiceParser.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3
+{
+    public static class DiceParser
+    {
+        public static List<Dice> Parse(string[] args)
+        {
+            if (args.Length < 3)
+                throw new ArgumentException("At least 3 dice must be provided.");
+
+            var diceList = new List<Dice>();
+            foreach (var arg in args)
+            {
+                diceList.Add(new Dice(arg));
+            }
+            return diceList;
+        }
+    }
+}
