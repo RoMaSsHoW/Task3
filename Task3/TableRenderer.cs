8346aaab8a94c03019b2154e824de2450c8b6c84
Roman Saharov <fantomfantomovich0@gmail.com>
2025-05-27 17:49:58 +0500

    123

--- Task3/TableRenderer.cs
+++ Task3/TableRenderer.cs
@@ -7,7 +7,7 @@ namespace Task3
         public static void RenderProbabilityTable(List<Dice> diceList)
         {
             Console.WriteLine("\nProbability Table:");
-            var table = new ConsoleTable(new string[] { "----Dices----" }.Concat(diceList.Select(d => d.ToString())).ToArray());
+            var table = new ConsoleTable(new string[] { "User dice v" }.Concat(diceList.Select(d => d.ToString())).ToArray());
             for (int i = 0; i < diceList.Count; i++)
             {
                 var row = new List<string> { diceList[i].ToString() };
