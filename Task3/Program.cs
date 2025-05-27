80462f2fb8981846be937f47a2feac8217d78749
Roman Saharov <fantomfantomovich0@gmail.com>
2025-05-27 21:06:11 +0500

    123

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -13,7 +13,6 @@
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
-                Console.WriteLine("Usage: game.exe 2,2,4,4,9,9 6,8,1,1,8,6 7,5,3,7,5,3");
             }
         }
     }
